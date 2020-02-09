0a52e8784f568f23e14d8b5acd0ebdbec1607372
Sasnauskas Paulius <paulius.sasn@gmail.com>
2020-02-09 12:52:02 +0200

    feature/initial_upgrade Update and fix page text and layout

--- src/components/home/whatWeDo.vue
+++ src/components/home/whatWeDo.vue
@@ -28,27 +28,27 @@
                 <v-layout row wrap>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">chat_bubble</v-icon>
+                        <v-icon large style="font-size:300%">school</v-icon>
                         <p class="google-font" style="font-size:130%">Talks</p>
                         <p class="google-font" style="font-size:110%">Get updated with the latest news and announcements</p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">code</v-icon>
-                        <p class="google-font" style="font-size:130%">Codelabs</p>
-                        <p class="google-font" style="font-size:110%">Get hands-on experience and guidance from the community members</p>
+                        <p class="google-font" style="font-size:130%">Workshops</p>
+                        <p class="google-font" style="font-size:110%">Get hands-on experience and guidance from community members and professionals</p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">school</v-icon>
-                        <p class="google-font" style="font-size:130%">Campus Roadshows</p>
-                        <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
+                        <v-icon large style="font-size:300%">chat_bubble</v-icon>
+                        <p class="google-font" style="font-size:130%">Meetups</p><!-- Campus Roadshows -->
+                        <p class="google-font" style="font-size:110%">Make connections, find like minded people</p><!-- Share knowledge in different companies, colleges and universities -->
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">airplay</v-icon>
-                        <p class="google-font" style="font-size:130%">Live Viewing Parties</p>
-                        <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
+                        <p class="google-font" style="font-size:130%">Livestream Parties</p>
+                        <p class="google-font" style="font-size:110%">Be the first to know about technology news and developer products with other people from our community</p>
                     </v-flex>
 
                 </v-layout>
